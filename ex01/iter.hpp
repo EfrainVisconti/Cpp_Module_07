0ae03aad0173ab5b23e655b533bc5a2f184eecbe
Efrain Eduardo Visconti Vivas <eviscont@c4r1s6.42urduliz.com>
2024-11-24 21:23:33 +0100

    ex01 done, ex02 init

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,19 +6,34 @@
 /*   By: eviscont <eviscont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 17:48:53 by eviscont          #+#    #+#             */
-/*   Updated: 2024/11/23 17:58:57 by eviscont         ###   ########.fr       */
+/*   Updated: 2024/11/24 20:55:57 by eviscont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
-template <typename T>
-void	swap(T &first, T &second)
+#include <iostream>
+
+template <typename T, typename F>
+void	iter(T *arr, std::size_t len, F func)
 {
-	T aux = first;
-	first = second;
-	second = aux;
+	for (std::size_t i = 0; i < len; i++)
+		func(arr[i]);
 }
+
+// template <typename T>
+// void	iter(T *arr, std::size_t len, void (*func)(T))
+// {
+// 	for (std::size_t i = 0; i < len; i++)
+// 		func(arr[i]);
+// }
+
+// template <typename T>
+// void	iter(T *arr, std::size_t len, void (*func)(T&))
+// {
+// 	for (std::size_t i = 0; i < len; i++)
+// 		func(arr[i]);
+// }
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,13 +6,47 @@
 /*   By: eviscont <eviscont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 17:47:56 by eviscont          #+#    #+#             */
-/*   Updated: 2024/11/23 17:59:24 by eviscont         ###   ########.fr       */
+/*   Updated: 2024/11/24 21:09:23 by eviscont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
+void	intPrint(int nbr)
+{
+	std::cout << nbr << " ";
+}
+
+void	intUpdate(int &nbr)
+{
+	nbr *= 10;
+}
+
+void	strUpdate(std::string &str)
+{
+	str = "* " + str + " *";
+}
+
+void	strPrint(std::string str)
+{
+	std::cout << str << " ";
+}
+
 int	main(void)
 {
-	
+	int	arr1[5] = {1, 2, 3, 4, 5};
+	std::string	arr2[4] = {"hola", "hello", "ciao", "hallo"};
+
+	std::cout << "Original int array:" << std::endl;
+	::iter(arr1, 5, intPrint);
+	::iter(arr1, 5, intUpdate);
+	std::cout << std::endl << "Updated int array:" << std::endl;
+	::iter(arr1, 5, intPrint);
+	std::cout << std::endl << "Original str array:" << std::endl;
+	::iter(arr2, 4, strPrint);
+	::iter(arr2, 4, strUpdate);
+	std::cout << std::endl << "Updated str array:" << std::endl;
+	::iter(arr2, 4, strPrint);
+	std::cout << std::endl;
+	return 0;
 }
